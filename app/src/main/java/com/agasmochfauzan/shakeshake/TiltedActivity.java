262aaf7af52d04dc13c1116ef9b4778b1ce64247
agasmoch <agasm5@gmail.com>
2019-10-22 11:18:59 +0700

    Sensor Gerak Kanan-Kiri

---
+++ app/src/main/java/com/agasmochfauzan/shakeshake/TiltedActivity.java
@@ -0,0 +1,37 @@
+package com.agasmochfauzan.shakeshake;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+
+public class TiltedActivity extends AppCompatActivity {
+
+    private TiltedDetector mDetector;
+    private Sensor mAccelerometer;
+    private SensorManager mManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tilted);
+
+
+        mManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer= mManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mDetector = new TiltedDetector(new TiltedDetector.OnTiltedListener() {
+            @Override
+            public void leftTilted() {
+                Log.d("TILT","Left tilted");
+            }
+
+            @Override
+            public void rightTiled() {
+                Log.d("TILT","Right tilted");
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/agasmochfauzan/shakeshake/TiltedDetector.java
@@ -0,0 +1,33 @@
+package com.agasmochfauzan.shakeshake;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+
+public class TiltedDetector implements SensorEventListener {
+
+    private OnTiltedListener mListener;
+
+    interface OnTiltedListener {
+        void leftTilted();
+        void rightTiled();
+    }
+
+
+    public TiltedDetector(OnTiltedListener listener) {
+        this.mListener = listener;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.values[0] < 0)
+            mListener.leftTilted();
+        else
+            mListener.rightTiled();
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
